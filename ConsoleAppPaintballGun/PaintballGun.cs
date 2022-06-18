f24c48bbb9884f4bf1d91e84de83ee11c0e9cfc2
zerro <zerro.turbo@gmail.com>
2022-06-18 09:23:46 +0300

    Initial commit

---
+++ ConsoleAppPaintballGun/PaintballGun.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleAppPaintballGun;
+
+public class PaintballGun
+{
+    public const int MagazineSize = 16;
+    public int GetBalls { get; private set; } = 0;
+    public int GetBallsLoaded { get; private set; } = 0;
+    public bool IsEmpty => GetBallsLoaded == 0;
+    
+    public void SetBalls(int numberOfBalls)
+    {
+        if (numberOfBalls > 0)
+            GetBalls = numberOfBalls;
+        Reload();
+    }
+    
+    public void Reload()
+    {
+        GetBallsLoaded = GetBalls > MagazineSize ? MagazineSize : GetBalls;
+    }
+    
+    public bool Shoot()
+    {
+        if (GetBallsLoaded == 0) return false;
+        GetBallsLoaded--;
+        GetBalls--;
+        return true;
+    }
+}
---
+++ ConsoleAppPaintballGun/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppPaintballGun;
+
+var gun = new PaintballGun();
+
+while (true)
+{
+    Console.WriteLine($"{gun.GetBalls} balls, {gun.GetBallsLoaded} loaded");
+    if(gun.IsEmpty) Console.WriteLine("WARNING: You're out of ammo");
+    Console.WriteLine("Space to shoot, к to reload, + to add ammo, q to quit");
+    var key = Console.ReadKey(true).KeyChar;
+    
+    switch (key)
+    {
+        case ' ':
+            Console.WriteLine($"Shooting return {gun.Shoot()}");
+            break;
+        case 'r':
+            gun.Reload();
+            break;
+        case '+':
+            gun.SetBalls(gun.GetBalls + PaintballGun.MagazineSize);
+            break;
+        case 'q':
+            return;
+    }
+}
